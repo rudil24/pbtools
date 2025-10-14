fcefc5621434077409cf9a45a28a43b1633df482
Rudi Lewis <69172938+rudil24@users.noreply.github.com>
2025-10-14 16:36:14 -0600

    Update _PlayLists Home.sql

--- popper/_PlayLists Home.sql
+++ popper/_PlayLists Home.sql
@@ -1,7 +1,7 @@
 /* COMMENTS:
   PlayLists Home (MediaName: pl_home) is the opening screen of Popper. This script serves a 
   "SIX PACK" of random tables: one TOP 25 GameRating table from each of six categories: 
-  DMD PuP, DMD non-PuP, SS alpha, SS, EM, and Original (non-vendor) tables.
+  DMD PuP, DMD non-PuP, SS alpha, SS, EM, and Original (non-hardware-manufactured) tables.
 */
 SELECT * FROM (
     SELECT * FROM (
